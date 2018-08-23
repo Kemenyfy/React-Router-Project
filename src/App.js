71f98b7af5cb8aedf952ecaa91a67805c44e0eee
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-08-23 13:27:59 -0400

    I don't know what is going to happen with these

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Nav from './Components/Nav'
+// import Breadcrumbs from './Components/Breadcrumbs'
 import CategoryList from './Components/CategoryList'
 import PhotoList from './Components/PhotoList'
 import PhotoDetail from './Components/PhotoDetail'
@@ -10,20 +10,18 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <section className="App">
+        <div className="App">
           <header className='Header'>
-            <section>
               <h1>Things that I like</h1>
               <h5>A Photo Gallery made by somebody/somehow/maybe</h5>
-            </section>
-            <Nav />
+            {/* <Breadcrumbs/> */}
           </header>  
           <Switch>
             <Route path="/" exact component={CategoryList} />
             <Route path="/page1" exact component={PhotoList} />
             <Route path="/page2" exact component={PhotoDetail} />
           </Switch>
-        </section>
+        </div>
       </Router>
     );
   }
